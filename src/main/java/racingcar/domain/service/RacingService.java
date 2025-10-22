4cf62191b82e06512b9be6683d834fc385697aed
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-22 13:12:13 +0900

    test(service): 레이싱 서비스 테스트 스켈레톤 생성

--- src/main/java/racingcar/domain/service/RacingService.java
+++ src/main/java/racingcar/domain/service/RacingService.java
@@ -1,4 +1,7 @@
 package racingcar.domain.service;
 
+import racingcar.domain.vo.CarList;
+
 public interface RacingService {
+    CarList saveCarList();
 }
--- src/test/java/racingcar/domain/service/RacingServiceTest.java
+++ src/test/java/racingcar/domain/service/RacingServiceTest.java
@@ -1,4 +1,26 @@
 package racingcar.domain.service;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.entity.Car;
+import racingcar.domain.impl.RacingServiceImpl;
+import racingcar.domain.vo.CarList;
+
 public class RacingServiceTest {
+
+    private final RacingService racingService = new RacingServiceImpl(new String[]{"pobi", "woni", "jun"});
+
+    @Test
+    @DisplayName("차량목록생성_성공")
+    void saveCarList_success() {
+        Car car = new Car("pobi", 0);
+        List<Car> cars = new ArrayList<>();
+        cars.add(car);
+        CarList carListTest = new CarList(cars);
+        assertThat(racingService.saveCarList()).isEqualTo(carListTest);
+    }
 }
