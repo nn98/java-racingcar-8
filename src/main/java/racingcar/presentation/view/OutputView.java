744a0d297b71f452ffc1638f1e4fd80f98d0306f
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-25 08:12:49 +0900

    feat(output): 입력 안내문 출력

--- src/main/java/racingcar/presentation/view/OutputView.java
+++ src/main/java/racingcar/presentation/view/OutputView.java
@@ -1,9 +1,18 @@
 package racingcar.presentation.view;
 
+import racingcar.common.constant.Message;
+
 public class OutputView {
 
-    public void printInputNotice() {
-        System.out.println();
+    public void noticeCarName() {
+        System.out.println(Message.NOTICE_INPUT_CAR_NAME);
     }
 
+    public void noticeTryCount() {
+        System.out.println(Message.NOTICE_INPUT_TRY_COUNT);
+    }
+
+    public void noticeRaceResult() {
+        System.out.println(Message.NOTICE_RACE_RESULT);
+    }
 }
--- src/test/java/racingcar/presentation/view/OutputViewTest.java
+++ src/test/java/racingcar/presentation/view/OutputViewTest.java
@@ -13,11 +13,16 @@ public class OutputViewTest extends NsTest {
     @Test
     @DisplayName("입력_안내문_출력")
     void print_input_notice() {
-        assertThat(output()).contains("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)", "시도할 횟수는 몇 회인가요?");
+        run();
+        assertThat(output()).contains(
+                "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+                "시도할 횟수는 몇 회인가요?"
+        );
     }
 
     @Override
     public void runMain() {
-        outputView.printInputNotice();
+        outputView.noticeCarName();
+        outputView.noticeTryCount();
     }
 }
