c4d90614daf34168ed6f7e148aa11cfb8bbcf6b9
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-25 08:21:43 +0900

    feat(application): run  호출 구조

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,7 +3,9 @@ package racingcar;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        run();
     }
-    
+
+    public static void run() {
+    }
 }
