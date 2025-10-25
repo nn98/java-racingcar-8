6fd645994b877cf2cb0f71452afe42d4192b8093
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-25 09:39:51 +0900

    feat(car): 생성자 구현

--- src/main/java/racingcar/domain/entity/Car.java
+++ src/main/java/racingcar/domain/entity/Car.java
@@ -2,7 +2,12 @@ package racingcar.domain.entity;
 
 public class Car {
 
+    private final String carName;
+    private final int position;
+
     public Car(String name, int position) {
+        this.carName = name;
+        this.position = position;
     }
 
     public void move(int randomValue) {
