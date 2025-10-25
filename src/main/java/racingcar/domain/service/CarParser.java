fdf6c03b50134556d073fac2b0759ae7f427fbe4
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-25 09:03:48 +0900

    refactor(parser): 자동차 파싱 기능 개선 - 공백 관련

--- src/main/java/racingcar/domain/service/CarParser.java
+++ src/main/java/racingcar/domain/service/CarParser.java
@@ -1,9 +1,22 @@
 package racingcar.domain.service;
 
+import java.util.Arrays;
+
 public class CarParser {
 
-    public String[] parseCar(String s) {
-        return s.split(",");
+    public String[] parseCar(String input) {
+        checkValidInput(input);
+        String[] cars;
+        cars = Arrays.stream(input.split(","))
+                .map(String::trim)
+                .toArray(String[]::new);
+        return cars;
+    }
+
+    private void checkValidInput(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("입력값이 공백입니다.");
+        }
     }
 
 }
