f033c1a6700075500010b521f44574c130e529be
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-25 09:04:52 +0900

    refactor(parser): 자동차 파싱 기능 개선 - 입력값 확인

--- src/main/java/racingcar/domain/service/CarParser.java
+++ src/main/java/racingcar/domain/service/CarParser.java
@@ -10,12 +10,35 @@ public class CarParser {
         cars = Arrays.stream(input.split(","))
                 .map(String::trim)
                 .toArray(String[]::new);
+        checkValidCars(cars);
         return cars;
     }
 
     private void checkValidInput(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException("입력값이 공백입니다.");
+        }
+    }
+
+    private void checkValidCars(String[] cars) {
+        for (String car : cars) {
+            isValidLength(car);
+            isValidString(car);
+        }
+    }
+
+    private void isValidLength(String car) {
+        if (car.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름이 공백입니다.");
+        }
+        if (car.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름의 길이가 5를 초과합니다.");
+        }
+    }
+
+    private void isValidString(String car) {
+        if (car.contains("\\")) {
+            throw new IllegalArgumentException("자동차 이름에 escape sequence가 포함되어 있습니다.");
         }
     }
 
