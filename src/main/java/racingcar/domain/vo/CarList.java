cbf8db7f91c5117cccc62ed93f700bafe224c448
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-25 09:32:02 +0900

    feat(carList): 생성자 및 초기화

--- src/main/java/racingcar/domain/vo/CarList.java
+++ src/main/java/racingcar/domain/vo/CarList.java
@@ -1,8 +1,28 @@
 package racingcar.domain.vo;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.entity.Car;
+
 public class CarList {
 
+    private final List<Car> carList;
+
     public CarList(String[] cars) {
+        this.carList = getCarList(cars);
+    }
+
+    private List<Car> getCarList(String[] cars) {
+        List<Car> carList = new ArrayList<>();
+        for (String carName : cars) {
+            Car car = new Car(carName, 0);
+            carList.add(car);
+        }
+        return carList;
+    }
+
+    public String statusToString() {
+        return "";
     }
 
 }
