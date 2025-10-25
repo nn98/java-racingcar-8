dc5bd878a407e9ad4d8af741881f136f4d4a75be
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-25 18:10:06 +0900

    feat(racingService): 생성자로 carList 초기화

--- src/main/java/racingcar/domain/service/RacingService.java
+++ src/main/java/racingcar/domain/service/RacingService.java
@@ -4,10 +4,13 @@ import racingcar.domain.vo.CarList;
 
 public class RacingService {
 
+    private final CarList carList;
+
     public RacingService(String[] cars) {
+        this.carList = new CarList(cars);
     }
 
     public CarList saveCarList() {
-        return null;
+        return this.carList;
     }
 }
