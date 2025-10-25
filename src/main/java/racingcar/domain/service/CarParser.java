9bfcb8d86f4116891f76ba938d74971c4e00bde4
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-25 09:05:27 +0900

    refactor(parser): 자동차 파싱 기능 개선 - 중복값 확인

--- src/main/java/racingcar/domain/service/CarParser.java
+++ src/main/java/racingcar/domain/service/CarParser.java
@@ -1,6 +1,8 @@
 package racingcar.domain.service;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CarParser {
 
@@ -11,6 +13,7 @@ public class CarParser {
                 .map(String::trim)
                 .toArray(String[]::new);
         checkValidCars(cars);
+        checkDuplicatedCars(cars);
         return cars;
     }
 
@@ -39,6 +42,13 @@ public class CarParser {
     private void isValidString(String car) {
         if (car.contains("\\")) {
             throw new IllegalArgumentException("자동차 이름에 escape sequence가 포함되어 있습니다.");
+        }
+    }
+
+    private void checkDuplicatedCars(String[] cars) {
+        Set<String> set = new HashSet<>(Arrays.asList(cars));
+        if (set.size() != cars.length) {
+            throw new IllegalArgumentException("자동차 이름 중 중복값이 존재합니다.");
         }
     }
 
