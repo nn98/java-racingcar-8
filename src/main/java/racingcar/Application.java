67cb6dbaa4413191f94d6d534638f1e67c80d329
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-26 11:07:39 +0900

    feat(application): 컨트롤러 호출 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,22 +1,16 @@
 package racingcar;
 
+import racingcar.presentation.controller.RacingController;
 import racingcar.presentation.view.InputView;
 import racingcar.presentation.view.OutputView;
 
 public class Application {
 
-    private final InputView inputView = new InputView();
-    private final OutputView outputView = new OutputView();
-
     public static void main(String[] args) {
-        run();
-    }
-
-    public static void run() {
-        init();
-    }
-
-    public static void init() {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        RacingController controller = new RacingController(inputView, outputView);
+        controller.run();
     }
 
 }
