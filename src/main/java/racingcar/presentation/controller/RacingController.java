abc81924fa16f3859517d4cafe205508d8915ebc
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-26 11:08:33 +0900

    feat(controller): 주입받은 View 기반 서비스 초기화, 레이싱 시작

--- src/main/java/racingcar/presentation/controller/RacingController.java
+++ src/main/java/racingcar/presentation/controller/RacingController.java
@@ -1,4 +1,25 @@
 package racingcar.presentation.controller;
 
+import racingcar.domain.service.RacingService;
+import racingcar.presentation.view.InputView;
+import racingcar.presentation.view.OutputView;
+
 public class RacingController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RacingController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        RacingService racingService = initService();
+    }
+
+    private RacingService initService() {
+        return new RacingService(inputView.readLine(), inputView.readInt());
+    }
+
 }
