7676780fe4ab45425afc2329606b97437bc653f1
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-26 10:55:03 +0900

    feat(application): 기초 동작 및 입출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,22 @@
 package racingcar;
 
+import racingcar.presentation.view.InputView;
+import racingcar.presentation.view.OutputView;
+
 public class Application {
+
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
 
     public static void main(String[] args) {
         run();
     }
 
     public static void run() {
+        init();
     }
+
+    public static void init() {
+    }
+
 }
--- src/main/java/racingcar/presentation/view/InputView.java
+++ src/main/java/racingcar/presentation/view/InputView.java
@@ -8,4 +8,12 @@ public class InputView {
         return Console.readLine();
     }
 
+    public int readInt() {
+        try {
+            return Integer.parseInt(readLine());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("숫자 값을 입력해주세요.");
+        }
+    }
+
 }
