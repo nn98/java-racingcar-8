06da3d57f7dac22609a82195a5f9982541407b39
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-26 11:35:59 +0900

    feat(outputView): 실행 결과 출력 수정

--- src/main/java/racingcar/presentation/view/OutputView.java
+++ src/main/java/racingcar/presentation/view/OutputView.java
@@ -4,6 +4,12 @@ import racingcar.common.constant.Message;
 
 public class OutputView {
 
+    private boolean firstResultNotice;
+
+    public OutputView() {
+        this.firstResultNotice = true;
+    }
+
     public void noticeCarName() {
         System.out.println(Message.NOTICE_INPUT_CAR_NAME);
     }
@@ -12,7 +18,11 @@ public class OutputView {
         System.out.println(Message.NOTICE_INPUT_TRY_COUNT);
     }
 
-    public void noticeRaceResult() {
-        System.out.println(Message.NOTICE_RACE_RESULT);
+    public void noticeRaceResult(String raceResult) {
+        if (firstResultNotice) {
+            System.out.println(Message.NOTICE_RACE_RESULT);
+            firstResultNotice = false;
+        }
+        System.out.println(raceResult + "\n");
     }
 }
