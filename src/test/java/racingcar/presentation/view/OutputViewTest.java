d343aba1068d0a7febfb8ac4302f039196d8ad62
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-27 10:56:39 +0900

    test(output): 최종 우승자 출력 테스트 추가

--- src/test/java/racingcar/presentation/view/OutputViewTest.java
+++ src/test/java/racingcar/presentation/view/OutputViewTest.java
@@ -6,7 +6,7 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class OutputViewTest extends NsTest {
+class OutputViewTest extends NsTest {
 
     private final OutputView outputView = new OutputView();
 
@@ -16,7 +16,8 @@ public class OutputViewTest extends NsTest {
         run();
         assertThat(output()).contains(
                 "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
-                "시도할 횟수는 몇 회인가요?"
+                "시도할 횟수는 몇 회인가요?",
+                "최종 우승자 : pobi, jun"
         );
     }
 
@@ -24,5 +25,6 @@ public class OutputViewTest extends NsTest {
     public void runMain() {
         outputView.noticeCarName();
         outputView.noticeTryCount();
+        outputView.noticeRaceWinner("pobi, jun");
     }
 }
