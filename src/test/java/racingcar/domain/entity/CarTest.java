84f7c32c14cdc664e89f74be0d3f53dd29e17cc4
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-27 10:40:52 +0900

    test(car): getter 추가로 인한 수정

--- src/test/java/racingcar/domain/entity/CarTest.java
+++ src/test/java/racingcar/domain/entity/CarTest.java
@@ -5,23 +5,21 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class CarTest {
+class CarTest {
 
     @Test
     @DisplayName("이동_성공")
     void move_success() {
         Car car = new Car("pobi", 0);
-        String original = car.toString();
         car.move(4);
-        assertThat(car.toString()).doesNotHaveToString(original);
+        assertThat(car.getPosition()).isEqualTo(1);
     }
 
     @Test
     @DisplayName("부동_성공")
     void hold_success() {
         Car car = new Car("pobi", 0);
-        String original = car.toString();
         car.move(0);
-        assertThat(car.toString()).hasToString(original);
+        assertThat(car.getPosition()).isZero();
     }
 }
